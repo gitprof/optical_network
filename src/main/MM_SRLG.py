b09ad9de2809cc3b734e0552fab5e64d04b0521a
gitprof <mrlevis21@gmail.com>
2016-04-14 20:58:05 +0300

    Title: mm_srlg algorithm done Description:

--- src/main/MM_SRLG.py
+++ src/main/MM_SRLG.py
@@ -1,41 +1,293 @@
 
+import networkx as nx
+import matplotlib.pyplot as plt
+import itertools
+from pulp import *
+#from scipy.optimize import linprog
+import numpy as np
+import imp
 import math
 import copy
+import os
+import copy
+from OpticalNetwork import LogicalNetwork
+from OpticalNetwork import OpticalNetwork
 
-class MM_SRLG:
+Global = imp.load_source('Global', os.path.join('src', 'main', 'Global.py'))
+from Global import *
+
+Debug = imp.load_source('Debug', os.path.join(MAIN_DIR, 'utilities', 'Debug.py'))
+
+
+'''
+MM_SRLG:
+    * there are 3 helper algorithm, and the main algorithm.
+    * every algorithm that modify the original graph should deep copy it
+'''
+class MM_SRLG_solver:
     def __init__(self):
+        self.debug = register_debugger('MM_SRLG')
 
-
-
-    def alg(self):
-
+    def ALG(self, opt_network):
+        logical_network = LogicalNetwork()
+        return logical_network
 
     def mm_srlg_arb(self, opt_network):
-        half_b = math.ceil()
+        self.debug.assrt(opt_network != None, 'mm_slrg_arb: optical_network is None!')
+        # print(opt_network.graph.edges())
+        half_b = int(math.ceil(opt_network.B/2))
+        logical_network = None
+        print('mm_srlg_arb: half_b = '+str(half_b))
         for C in range(1, half_b+1):
-            new_c = min(C, )
+            print('C: '+str(C))
+            opt_network.set_strict_capacity(C)
+            logical_network = self.ALG(opt_network)            
+            if len(logical_network.paths) >= opt_network.B:
+                break
+        self.debug.assrt(logical_network != None, 'mm_slrg_arb: logical_network is None!')
+        logical_network.choose_arbitrary_subset_of_size_b(opt_network.B)
+        self.debug.logger('mm_srlg_arb: produced logical network: ')
+        self.debug.logger(logical_network.paths)
+        return logical_network
+
+    def produce_cycle_aux(self, tree, cycle, ans, node):
+        cycle.append((node, True))
+        print('cycle_debug = ', cycle)
+        next_neighbors = tree.neighbors(node)
+        print('nbrs: ', node, '-', next_neighbors)
+        if ans != node:
+            next_neighbors.remove(ans)
+        for neighbor in next_neighbors:
+            cycle = self.produce_cycle_aux(tree, cycle, node, neighbor)
+            cycle.append((node, False))
+        return cycle
+
+    def produce_cycle_from_tree(self, opt_network, tree):
+        cycle = []
+        start_node = None
+        for node in opt_network.nodes():
+            if opt_network.is_logical_node(node):
+                start_node = node
+                break
+        self.debug.assrt(start_node is not None, "produce_cycle_from_tree: start node is None")
+        self.produce_cycle_aux(tree, cycle, start_node, start_node)
+        cycle.pop()
+        cycle.append((start_node, True))
+        self.debug.assrt((cycle != None) and (len(cycle) > 0), 'produce_cycle_from_tree: bad cycle')
+        print("cycle")
+        return cycle
+
+    ''' 
+    Params:
+     cycle: list of nodes, first and last are the same logical node. all the logical nodes should be in the cycle
+                all nodes are UNIQUE, except first and last
+    '''
+    def produce_subpaths_from_cycle(self, opt_network, cycle):
+        curr_path = []
+        paths = []
+        last_logical_node = None
+        first_node = True
+        for (node, is_end_point) in cycle:
+            curr_path.append(node)
+            if first_node:
+                first_node = False
+                continue
+            if opt_network.is_logical_node(node) and is_end_point:
+                paths.append(curr_path)
+                curr_path = [node]
+        return paths
+
 
     def mm_srlg_cycle(self, opt_network):
-        opt_network.create_spanning_tree()
-        leaves = [node for node in opt_network if ((opt_network.node_degree(node) == 1) and (not opt_network.is_logical_node(node)))]
-        while leaves is not []:
+        spanning_tree = opt_network.create_spanning_tree()
+        # print("mm_srlg_cycle:")
+        # print(opt_network.get_logical_nodes())
+        leaves = [node for node in spanning_tree.nodes() if ((spanning_tree.degree(node) == 1) and (not opt_network.is_logical_node(node)))]
+        #self.debug.assrt((leaves != None) and (len(leaves) > 0), 'mm_srlg: no leaves!')
+        while leaves != []:
             leaf = leaves.pop()
-            neighbors = opt_network.node_neighbours(leaf)
-            opt_network.remove_node(leaf)
+            neighbors = spanning_tree.neighbors(leaf) 
+            spanning_tree.remove_node(leaf)
             leaves = leaves + [node for node in neighbors if ((opt_network.node_degree(node) == 1) and (not opt_network.is_logical_node(node)))]
-        cycle_list       = opt_network.produce_cycle_from_tree()
-        logical_netowork = LogicalNetwork().init_from_cycle(cycle_list)
+        cycle = self.produce_cycle_from_tree(opt_network, spanning_tree)
+        self.debug.logger('mm_srlg_cycle: cycle:')
+        self.debug.logger(cycle)
+        paths = self.produce_subpaths_from_cycle(opt_network, cycle)
+        self.debug.assrt((paths != None) and (len(paths) > 0), 'mm_srlg_cycle: bad paths!')
+        logical_network = LogicalNetwork().init_from_paths(paths)
+        self.debug.logger('mm_srlg_cycle: produced logical network: ')
+        self.debug.logger(logical_network.paths)
         return logical_network
 
     def mm_srlg(self, opt_network):
-        cycle_net = copy.deecopy(opt_network)
-        EL_cycle = self.mm_srlg_cycle(cycle_net)
-        for e in cycle_net.physical_links():
-            num_lightpaths = cycle_net.num_lightpahts_via_e(e)
+        cycle_net = opt_network.clone()
+        self.debug.assrt((opt_network != None) and (opt_network.B == cycle_net.B), 'mm_slrg: opt_network invalid!')
+        print('mm_srlg:')
+        print(opt_network.get_logical_nodes())
+        print(cycle_net.get_logical_nodes())
+        EL_cycle  = self.mm_srlg_cycle(cycle_net)
+        for e in sorted(opt_network.physical_links().keys()):
+            num_lightpaths = EL_cycle.num_lightpaths_via_e(e)
             new_capacity   = cycle_net.get_plink_capacity(e) - num_lightpaths
-            cycle_net.set_plink_capacity(e, new_capacity)
-        opt_network.B = opt_network.B - opt_network.num_logical_nodes()
-        EL_arb        = self.mm_srlg_arb(opt_network)
-        return EL_arb.merge(EL_cycle)
+            cycle_net.set_edge_capacity(e, new_capacity)
+        opt_network.B = opt_network.B - len(opt_network.get_logical_nodes())
+        print('mm_srlg: B left after cycle = '+str(opt_network.B))
+        EL_arb        = self.mm_srlg_arb(opt_network) if (opt_network.B > 0) else LogicalNetwork()
+        EL_arb.merge(EL_cycle)
+        return EL_arb
+    
+    ''' Here we assume only 1 logical path between 2 routers
+    '''
+    def process_alg_output(self, prob, routers, nodes, edges): 
+        # self.debug.logger('process_alg_output:')
+        self.debug.assrt("Optimal" == LpStatus[prob.status], "process_alg_output: ALG failed to find optimal solution!")
         
+        paths_indicators = {}
+        links_indicators = {}
+        for v in prob.variables():
+            s = v.name.split('_')
+            if 'r' == s[0]:
+                links_indicators[(int(s[2]), int(s[3]), int(s[4]), int(s[5]))] = v.varValue
+            if 'l' == s[0]:
+                paths_indicators[(int(s[1]), int(s[2]))]                       = v.varValue
+                    
+        logical_paths = []
+        #print(routers)
+        #print(nodes)        
+        # print('paths_indicators:')                
+        # print(paths_indicators)
+        
+        for r1 in routers:
+            for r2 in routers:
+                if r1 >= r2 or paths_indicators[(r1,r2)] == 0:
+                    continue
+                curr_node = r1
+                curr_path = [curr_node]
+                while curr_node != r2:
+                    next_node = None
+                    for n in nodes:
+                        try:
+                            if (((curr_node, n) in edges) or ((n, curr_node) in edges)) and links_indicators[(r1,r2,curr_node,n)] > 0:
+                                next_node = n
+                                break
+                        except KeyError:
+                            donothing = 0
+                    self.debug.assrt(next_node != None, "process_alg_output: didnt find next node in path!")
+                    curr_path.append(next_node)
+                    links_indicators[(r1,r2,curr_node,next_node)] = 0
+                    curr_node = next_node
+                    
+                logical_paths.append(curr_path)                                                           
+                paths_indicators[(r1,r2)] = 0
+        self.debug.assrt(value(prob.objective) == len(logical_paths), "process_alg_output: didnt find enough logical paths!")
+        return logical_paths
+
+        
+    def ALG(self, opt_network):
+        self.debug.logger('ALG:')
+        V_p            = opt_network.nodes()
+        physical_links = opt_network.physical_links()
+        E_p            = physical_links.keys()
+        V_l            = opt_network.get_logical_nodes()
+        edge_to_capacity = copy.deepcopy(physical_links)
+        for (i,j) in physical_links.keys():
+            edge_to_capacity[(j,i)] = physical_links[(i,j)]        
+        logical_paths = self.ALG_inner(V_p, E_p, V_l, edge_to_capacity)        
+        logical_network = LogicalNetwork()
+        logical_network.init_from_paths(logical_paths)
+        self.debug.logger('ALG: produced logical network: ')
+        self.debug.logger(logical_network.paths)
+        return logical_network   
+
+   
+    def ALG_inner(self, V_p, E_p, V_l, edge_to_capacity):
+        # print("\nALG_inner:")
+        # print(V_p)
+        # print(E_p)
+        # print(V_l)
+
+        #create two networkX graphs
+        G = nx.Graph()
+        G_log = nx.Graph()
+
+        G.add_nodes_from(V_p)
+        G.add_edges_from(E_p)
+
+        G_log.add_nodes_from(V_l)
+        #G_log is the logical graph(clique)
+        G_log.add_edges_from(list(itertools.combinations(V_l ,2)))
+
+        #debug - draw graph
+        #nx.draw(G)
+        #plt.show()
+
+        #add l_u_v variables as var attributes on G_log's edges
+        for (_u,_v) in G_log.edges():
+            u = min(_u, _v)
+            v = max(_u, _v)
+            G_log[u][v]['l_var'] = LpVariable("l_%d_%d" % (u,v), 0, 1,LpInteger)
+            G_log[u][v]['r_vars'] = {}
+
+        #add r_e_i_j_u_v variables as r_vars dictionary on G's edges
+        #r_e_i_j_u_v meaning - is the logical link u,v passes on the physical link i,j
+        for i,j in G.edges():
+            G[i][j]['c'] = edge_to_capacity[(i,j)]
+            G[i][j]['r_vars'] = {}
+            for _u,_v in G_log.edges():
+                u = min(_u, _v)
+                v = max(_u, _v)
+                #add 2 r_e vars one for i,j the other for j,i
+                var1 = LpVariable("r_e_%d_%d_%d_%d" % (u,v,i,j), 0, 1,LpInteger)
+                var2 = LpVariable("r_e_%d_%d_%d_%d" % (u,v,j,i), 0, 1,LpInteger)
+                #add reference both from G and G_log
+                G[i][j]['r_vars']['%d_%d' % (u,v)]  = [var1, var2]
+                G_log[u][v]['r_vars']['%d_%d' % (i,j)] = var1
+                G_log[u][v]['r_vars']['%d_%d' % (j,i)] = var2                
+
+        #init problem as a maximiztion problem using Pulp
+        prob = LpProblem("ALG",LpMaximize)
+        L_var = LpVariable("L",cat = LpInteger)
+
+        #add objective function to prob
+        prob += L_var
+
+        #add capacity constraints
+        for i,j in G.edges():
+            r_vars = []
+            for (var1,var2) in G[i][j]['r_vars'].values():
+                r_vars.extend([var1,var2])
+            prob += sum(r_vars) <= G[i][j]['c'], "%d_%d capacity constaraint" % (i,j)
+
+        #add logical link number constraint
+        prob += sum(G_log[u][v]['l_var'] for u,v in G_log.edges()) == L_var, "L_var constraint"
+
+        #add valid path constraints
+        for _u,_v in G_log.edges():
+            u = min(_u, _v)
+            v = max(_u, _v)
+            for i in G.nodes():
+                edges_in = []
+                edges_out = []
+                for j in G.neighbors(i):
+                    edges_in.append(G_log[u][v]['r_vars']['%d_%d' % (j,i)])
+                    edges_out.append(G_log[u][v]['r_vars']['%d_%d' % (i,j)])
+
+                if i == u:
+                    prob += sum(edges_in) - sum(edges_out) == - G_log[u][v]['l_var'],"path constarint (u,v,i) = (%d,%d,%d)" % (u,v,i)
+                elif i == v:
+                    prob += sum(edges_in) - sum(edges_out) ==  G_log[u][v]['l_var'],"path constarint (u,v,i) = (%d,%d,%d)" % (u,v,i)
+                else:
+                    prob += sum(edges_in) - sum(edges_out) == 0 ,"path constarint (u,v,i) = (%d,%d,%d)" % (u,v,i)
+
+        prob.solve()
+        
+        #print("Status:", LpStatus[prob.status])
+        # The optimised objective function value is printed to the screen
+        #print("L = ", value(prob.objective))
+        # Each of the variables is printed with it's resolved optimum value
+        #for v in prob.variables():
+        #    print(v.name, "=", v.varValue)
             
+        # print("ALG_inner: end")
+        logical_paths = self.process_alg_output(prob, V_l, V_p, E_p)
+        return logical_paths
--- src/main/OpticalNetwork.py
+++ src/main/OpticalNetwork.py
@@ -1,8 +1,17 @@
 #!/bin/python
 
 import networkx as nx
-from sets import Set
+import networkx.drawing as nxd
+import matplotlib.pyplot as plt
+import random
+import copy
+import imp
+import os
 from collections import Counter
+
+Global  = imp.load_source('Global',         os.path.join('src', 'main', 'Global.py'))
+from Global import *
+
 '''
 Graph attributes:
  edges attrs: 
@@ -15,8 +24,11 @@ class OpticalNetwork:
     def __init__(self):
         self.graph = nx.Graph()
         self.B     = 0 # default
-        self.l_net = LogicalNetwork()
+        #self.l_net = LogicalNetwork()
         #self.l_net = Set()
+        self.debug = register_debugger()
+        self.logical_nodes = []
+        self.input_graph = None
 
     ''' 
     Assume file of format:
@@ -30,38 +42,64 @@ class OpticalNetwork:
         (x is number)
     '''
     def init_graph_from_file(self, graph_file):
-        with open(self.graph_file, 'r') as f:
-            lines = [tuple(i.split(',')) for i in f]
-        self.B = lines[0][1]
-        edges = [(line[0],line[1],{'capacity':line[2]}) for line in lines]
-        self.graph.add_edges_from(edges)
-        self.logical_nodes = [node for node in self.graph.nodes() if self.is_logical_node(node)]
-        #self.l_net.set_from_tuples()
+        print('init_graph_from_file')
+        with open(graph_file, 'r') as f:
+            lines = [tuple(i[:-1].split(',')) for i in f]
+        self.B = int(lines[0][1])
+        edges_lines = lines[1:]
+        input_edges = {(line[0], line[1]):int(line[2]) for line in edges_lines}
+        #input_nodes = []
+        self.logical_nodes = []
+        for edge in input_edges.keys():
+            for i in [0,1]:
+                if not (int(edge[i][1:]) in self.logical_nodes):                    
+                    if 'r' == edge[i][0]:
+                        self.logical_nodes.append(int(edge[i][1:]))
+        weighted_edges = [(int(edge[0][1:]), int(edge[1][1:]), {'capacity':input_edges[edge]}) for edge in input_edges.keys()]     
+        self.graph.add_edges_from(weighted_edges)
+        self.input_graph = copy.deepcopy(self.graph)                
+        
+    def clone(self):
+        print('clone:')
+        new_copy               = OpticalNetwork()
+        new_copy.B             = copy.deepcopy(self.B)        
+        new_copy.graph         = copy.deepcopy(self.graph)
+        new_copy.logical_nodes = copy.deepcopy(self.logical_nodes)
+        new_copy.debug         = self.debug
+        return new_copy
+        
+    
+    def verify_file_lines():
+        demo=0
 
     def set_edge_capacity(self, edge, capacity):
         self.graph[edge[0]][edge[1]]['capacity'] = capacity 
 
     def set_edge_logical_links(self, edge, logical_links):
         res = False
-        if self.graph[edge[0][edge[1]['capacity'] >= logical_links:
-            self.graph[edge[0]][edge[1]]['logiclinks'] = logical_links 
+        if (self.graph[edge[0]][edge[1]]['capacity'] >= logical_links):
+            self.graph[edge[0]][edge[1]]['logiclinks'] = logical_links
             res = True
         return res
+        
+    def is_edge(self, formatted_edges, n1, n2):
+        return ((n1, n2) in formatted_edges) or ((n2, n1) in formatted_edges)
 
-    def is_logical_node(_node):
-        return _node[0] == 'r'
+    def is_logical_node(self, _node):
+        return _node in self.logical_nodes
 
     def physical_links(self):
-         return self.graph.edges()
+        p_links = {(u,v):(self.graph[u][v]['capacity']) for (u,v) in self.graph.edges()} 
+        return p_links
 
     def nodes(self):
-        return self.graph.nodes()
+        return sorted(self.graph.nodes())
 
-    def logical_nodes(self):
-        return self.logical_nodes
+    def get_logical_nodes(self):
+        return sorted(self.logical_nodes)        
 
-    def logical_links(self):
-        #return self.l_net.get_as
+    #def logical_links(self):
+    #    return self.l_net.
 
     '''
     get spanning tree that spans VL.
@@ -70,52 +108,111 @@ class OpticalNetwork:
     return type: nx.Graph()
     '''
     def create_spanning_tree(self):
-        return minimum_spanning_tree(self.graph, 'capacity')
+        return nx.minimum_spanning_tree(self.graph, 'capacity')
 
     def remove_node(self, node):
         graph.remove_node(node)
 
     def node_degree(self, node):
-        return graph.degree(node)
+        return self.graph.degree(node)
 
     def node_neighbors(self, node):
-        return graph.neighbors(node)
+        return sorted(graph.neighbors(node))
 
-    def produce_cycle_aux(self, node, cycle_list):
-        cycle_list.append(node)
-        for neighbor in self.graph.neighbors(node):
-            produce_cycle_aux(neighbor, cycle_list)
+    def get_plink_capacity(self, edge):
+        return self.graph[edge[0]][edge[1]]['capacity']
 
-    def produce_cycle_from_tree(self):
-        cycle_list = []
-        start_node = None
-        for node in self.nodes():
-            if self.is_logical_node(node):
-                start_node = node
-        debug.assrt(start_node is not None, "produce_cycle_from_tree: start node is None")
-        produce_cycle_aux(start_node, cycle_list)
-        cycle_list.append(start_node)
-        return cycle_list
+    def set_strict_capacity(self, C):
+        # print(self.graph.edges())
+        for edge in self.graph.edges():
+            self.graph[edge[0]][edge[1]]['capacity'] = min(C, self.input_graph[edge[0]][edge[1]]['capacity'])
+
+    #def num_lightpaths_via_e(self, edge):
+    #    return self.l_net.num_lightpaths_via_e(edge)
+
+    def draw(self):
+        g = self.graph
+        pos = nx.spring_layout(self.graph,scale=2)
+        nx.draw_networkx_nodes(g, pos, nodelist=g.nodes(), node_color='r', node_size=500, alpha=0.8)
+        nx.draw_networkx_edges(g, pos, edgelist=g.edges(),  edge_color=[( (float(g[u][v]['capacity'])*0.01)+10000 ) for (u,v) in g.edges()], edge_vmin=100, edge_vmax=1000, width=5, alpha=0.8)
+        #nx.draw(self.graph,pos,font_size=8)
+        #nxd.draw(self.graph)
+        labels = {}
+        for node in g.nodes():
+            labels[node] = str(node)
+        nx.draw_networkx_labels(g, pos, labels, font_size=10)
+        #nx.draw(g)
+        #plt.draw()
+        plt.axis('off')
+        plt.show()
+
 
 class LogicalNetwork:
     def __init__(self):
         ''' 
             links: this is a dict: 
-                edge: num_logical_links.
-                while the end point of edge can be any 2 nodes
+             - edge: num_logical_paths_traversing_e.
+             # while the end point of edge can be any 2 nodes
             paths: list of paths.
-                path: list of nodes, s.t. that start node and the end node are logical nodes, 
-                    and all the rest are non-logical nodes and unique.
+             - path: list of nodes v1, v2 ... , vk.  s.t: v1 and vk are logical nodes.
+                                                   and all the rest can be logical or non-logical nodes.
+                                                   all nodes unique.
         '''
-        links = {}
-        paths = []
-    
-    def produce_paths_from_links(self):
-        #TODO
+        self.debug = register_debugger('opticalNetwork')
+        self.links = {}
+        self.paths = []
+        self.max_SRLG = None
+        self.initialized = False    
+        
+    def choose_arbitrary_subset_of_size_b(self, B):
+        self.debug.assrt(len(self.paths) >= B, "choose_an_arb_subset_of_size_B: not enough paths!")
+        random.seed()
+        sample_size = min(B, len(self.paths)) #TODO: check if can happen
+        #self.paths = random.sample(self.paths, sample_size)
+        self.paths = self.paths[0:sample_size]
+        self.init_from_paths(self.paths)
 
-    def merge(logical_network):
-        self.links = dict(Counter(logical_links) + Counter(logical_network.links))
-        self.produce_paths_from_links()
+    def calc_max_SRLG(self):
+        max_srlg = 0
+        max_link = None
+        for link in self.links.keys():
+            if self.links[link] > max_srlg:
+                max_srlg = self.links[link]
+                max_link = link
+        self.debug.assrt(max_link != None, "calc_max_SRLG: didnt find max SRLG!")
+        self.max_SRLG = max_srlg
+        
+    def get_max_SRLG(self):
+        self.calc_max_SRLG()
+        return self.max_SRLG   
 
-    def init_from_cycle(self, cycle):
-        #TODO
+    def merge(self, logical_network):
+        self.paths = self.paths + logical_network.paths
+        self.init_from_paths(self.paths)
+
+    def init_from_paths(self, paths):
+        self.paths = paths
+        self.links = {}
+        for path in paths:
+            self.debug.assrt(len(path) > 1, 'init_from_paths: path with length 1')
+            for i in range(0, len(path) - 1):
+                edge = (path[i], path[i+1])
+                (i,j) = (min(edge[0], edge[1]), max(edge[0], edge[1]))                
+                if (i,j) in self.links.keys():
+                    self.links[(i,j)] += 1 
+                else:
+                    self.links[(i,j)] =  1
+        self.initialized = True
+        return self
+
+    '''
+        we assume here that num_lightpaths_via_e = num_logicallinks_over_e
+    '''
+    def num_lightpaths_via_e(self, edge):
+        self.init_from_paths(self.paths)
+        try:
+            res = self.links[edge]
+        except KeyError:
+            res = 0
+        return res
+
