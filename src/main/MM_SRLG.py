6d4ec3a455ac21c798f55e1653b9908b1c874933
gitprof <mrlevis21@gmail.com>
2016-04-14 10:42:12 +0300

    Title: added LogicalNetwork class, and basic algorithm Description: in progress

---
+++ src/main/MM_SRLG.py
@@ -0,0 +1,41 @@
+
+import math
+import copy
+
+class MM_SRLG:
+    def __init__(self):
+
+
+
+    def alg(self):
+
+
+    def mm_srlg_arb(self, opt_network):
+        half_b = math.ceil()
+        for C in range(1, half_b+1):
+            new_c = min(C, )
+
+    def mm_srlg_cycle(self, opt_network):
+        opt_network.create_spanning_tree()
+        leaves = [node for node in opt_network if ((opt_network.node_degree(node) == 1) and (not opt_network.is_logical_node(node)))]
+        while leaves is not []:
+            leaf = leaves.pop()
+            neighbors = opt_network.node_neighbours(leaf)
+            opt_network.remove_node(leaf)
+            leaves = leaves + [node for node in neighbors if ((opt_network.node_degree(node) == 1) and (not opt_network.is_logical_node(node)))]
+        cycle_list       = opt_network.produce_cycle_from_tree()
+        logical_netowork = LogicalNetwork().init_from_cycle(cycle_list)
+        return logical_network
+
+    def mm_srlg(self, opt_network):
+        cycle_net = copy.deecopy(opt_network)
+        EL_cycle = self.mm_srlg_cycle(cycle_net)
+        for e in cycle_net.physical_links():
+            num_lightpaths = cycle_net.num_lightpahts_via_e(e)
+            new_capacity   = cycle_net.get_plink_capacity(e) - num_lightpaths
+            cycle_net.set_plink_capacity(e, new_capacity)
+        opt_network.B = opt_network.B - opt_network.num_logical_nodes()
+        EL_arb        = self.mm_srlg_arb(opt_network)
+        return EL_arb.merge(EL_cycle)
+        
+            
--- src/main/OpticalNetwork.py
+++ src/main/OpticalNetwork.py
@@ -1,29 +1,121 @@
 #!/bin/python
 
 import networkx as nx
-
-
+from sets import Set
+from collections import Counter
 '''
 Graph attributes:
- edges capacity: attr weight of the edge
+ edges attrs: 
+    - capacity
+    - logiclinks
  logical nodes: vertex marked with attr type = logical
  logical links: edges  marked with attr logical = <num_of_logical_links_on_the_physical>
 '''
 class OpticalNetwork:
-    def __init__(self, graph_file_in):
-        graph_file = graph_file_in
-        graph = nx.Graph()
-        B     = 0 # defau;t
+    def __init__(self):
+        self.graph = nx.Graph()
+        self.B     = 0 # default
+        self.l_net = LogicalNetwork()
+        #self.l_net = Set()
 
-    def set_edges_capacity(self, capacity):
-        for edge in graph.edges:
-            edge['weight'] = capacity
+    ''' 
+    Assume file of format:
+    B,<B_val>
+    <node1>,<node2>,<capacity>
+              ...
 
-    def edges(self):
-         return graph.edges()
+    while:
+        switch is noted by sx 
+        router is noted by rx
+        (x is number)
+    '''
+    def init_graph_from_file(self, graph_file):
+        with open(self.graph_file, 'r') as f:
+            lines = [tuple(i.split(',')) for i in f]
+        self.B = lines[0][1]
+        edges = [(line[0],line[1],{'capacity':line[2]}) for line in lines]
+        self.graph.add_edges_from(edges)
+        self.logical_nodes = [node for node in self.graph.nodes() if self.is_logical_node(node)]
+        #self.l_net.set_from_tuples()
+
+    def set_edge_capacity(self, edge, capacity):
+        self.graph[edge[0]][edge[1]]['capacity'] = capacity 
+
+    def set_edge_logical_links(self, edge, logical_links):
+        res = False
+        if self.graph[edge[0][edge[1]['capacity'] >= logical_links:
+            self.graph[edge[0]][edge[1]]['logiclinks'] = logical_links 
+            res = True
+        return res
+
+    def is_logical_node(_node):
+        return _node[0] == 'r'
+
+    def physical_links(self):
+         return self.graph.edges()
 
     def nodes(self):
-        return graph.nodes()
+        return self.graph.nodes()
+
+    def logical_nodes(self):
+        return self.logical_nodes
 
     def logical_links(self):
-        return
+        #return self.l_net.get_as
+
+    '''
+    get spanning tree that spans VL.
+    we are using minimum spanning tree since this is the available method, 
+        but there is no need for minimum, or the capacity attr
+    return type: nx.Graph()
+    '''
+    def create_spanning_tree(self):
+        return minimum_spanning_tree(self.graph, 'capacity')
+
+    def remove_node(self, node):
+        graph.remove_node(node)
+
+    def node_degree(self, node):
+        return graph.degree(node)
+
+    def node_neighbors(self, node):
+        return graph.neighbors(node)
+
+    def produce_cycle_aux(self, node, cycle_list):
+        cycle_list.append(node)
+        for neighbor in self.graph.neighbors(node):
+            produce_cycle_aux(neighbor, cycle_list)
+
+    def produce_cycle_from_tree(self):
+        cycle_list = []
+        start_node = None
+        for node in self.nodes():
+            if self.is_logical_node(node):
+                start_node = node
+        debug.assrt(start_node is not None, "produce_cycle_from_tree: start node is None")
+        produce_cycle_aux(start_node, cycle_list)
+        cycle_list.append(start_node)
+        return cycle_list
+
+class LogicalNetwork:
+    def __init__(self):
+        ''' 
+            links: this is a dict: 
+                edge: num_logical_links.
+                while the end point of edge can be any 2 nodes
+            paths: list of paths.
+                path: list of nodes, s.t. that start node and the end node are logical nodes, 
+                    and all the rest are non-logical nodes and unique.
+        '''
+        links = {}
+        paths = []
+    
+    def produce_paths_from_links(self):
+        #TODO
+
+    def merge(logical_network):
+        self.links = dict(Counter(logical_links) + Counter(logical_network.links))
+        self.produce_paths_from_links()
+
+    def init_from_cycle(self, cycle):
+        #TODO
